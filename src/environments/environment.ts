9f2aa482d26f905affd9b84584dfbc0c919d1857
Tommy Wong <tommywong821@gmail.com>
2022-07-06 21:46:54 +0800

    config prd env variable

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   app_url: 'https://ngok3fyp-frontend.herokuapp.com', //Example: https://myapp.com
-  cas_url: 'https://cas.ust.hk/cas', //Example: https://mycas.com/cas
+  cas_url: 'https://cas.ust.hk/cas/login', //Example: https://mycas.com/cas
   cas_validate_url: 'https://cas.ust.hk/cas/serviceValidate' //Example: https://mycas.com/cas/serviceValidate
 };
 
