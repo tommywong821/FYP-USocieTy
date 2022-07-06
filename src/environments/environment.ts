e393be10601caa71fa33c045216f616db459add3
Tommy Wong <tommywong821@gmail.com>
2022-07-06 21:42:34 +0800

    try cas server endpoint

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  app_url: 'https://ngok3fyp-frontend.herokuapp.com/', //Example: https://myapp.com
-  cas_url: 'https://cas.ust.hk/cas/login', //Example: https://mycas.com/cas
+  app_url: 'https://ngok3fyp-frontend.herokuapp.com', //Example: https://myapp.com
+  cas_url: 'https://cas.ust.hk/cas', //Example: https://mycas.com/cas
   cas_validate_url: 'https://cas.ust.hk/cas/serviceValidate' //Example: https://mycas.com/cas/serviceValidate
 };
 
