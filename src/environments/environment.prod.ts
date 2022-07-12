b516937bc89f52bf39c712c9509b5192ae308870
Tommy Wong <tommywong821@gmail.com>
2022-07-12 15:10:09 +0800

    fix cas env variable

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  app_url: 'https://ngok3fyp-frontend.herokuapp.com', //Example: https://myapp.com
+  app_url: 'https://ngok3fyp-frontend.herokuapp.com/', //Example: https://myapp.com
   cas_url: 'https://cas.ust.hk/cas', //Example: https://mycas.com/cas
   cas_validate_url: 'https://cas.ust.hk/cas/serviceValidate' //Example: https://mycas.com/cas/serviceValidate
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  app_url: 'https://ngok3fyp-frontend.herokuapp.com', //Example: https://myapp.com
+  app_url: 'https://ngok3fyp-frontend.herokuapp.com/', //Example: https://myapp.com
   cas_url: 'https://cas.ust.hk/cas', //Example: https://mycas.com/cas
   cas_validate_url: 'https://cas.ust.hk/cas/serviceValidate' //Example: https://mycas.com/cas/serviceValidate
 };
