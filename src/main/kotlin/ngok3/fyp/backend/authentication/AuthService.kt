1d15a0b452c6617f380d1295b71dbd6cd3a77d6c
Tommy Wong <tommywong821@gmail.com>
2022-07-18 18:37:21 +0800

    create auth api

---
+++ src/main/kotlin/ngok3/fyp/backend/authentication/AuthService.kt
@@ -0,0 +1,10 @@
+package ngok3.fyp.backend.authentication
+
+import org.springframework.stereotype.Service
+
+@Service
+class AuthService {
+    fun itscSSOLogin(): String {
+        return "Login";
+    }
+}
