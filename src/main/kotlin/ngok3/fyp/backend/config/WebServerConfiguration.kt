aaba369ece6fd666023c5a570d2121248552ca70
Tommy Wong <tommywong821@gmail.com>
2022-07-18 18:09:03 +0800

    add cors config for local frontend development

---
+++ src/main/kotlin/ngok3/fyp/backend/config/WebServerConfiguration.kt
@@ -0,0 +1,21 @@
+package ngok3.fyp.backend.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebServerConfiguration {
+    @Bean
+    fun addCorsConfig(): WebMvcConfigurer {
+        return object : WebMvcConfigurer {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                registry.addMapping("/**")
+                    .allowedMethods("*")
+                    .allowedOriginPatterns("http://localhost:4200")
+                    .allowCredentials(true)
+            }
+        }
+    }
+}
