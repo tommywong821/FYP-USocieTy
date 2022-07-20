6abdae1f720815ac93e4d2931ba4e2a543559eff
Tommy Wong <tommywong821@gmail.com>
2022-07-20 18:38:13 +0800

    finish send service validate to itsc sso

--- src/main/kotlin/ngok3/fyp/backend/authentication/AuthController.kt
+++ src/main/kotlin/ngok3/fyp/backend/authentication/AuthController.kt
@@ -3,6 +3,7 @@ package ngok3.fyp.backend.authentication
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -11,8 +12,8 @@ class AuthController(
     @Autowired val authService: AuthService
 ) {
     @GetMapping
-    @RequestMapping("/login")
-    fun itscSSOLogin(): String {
-        return this.authService.itscSSOLogin();
+    @RequestMapping("/serviceValidate")
+    fun itscSSOServiceValidate(@RequestParam("ticket") ticket: String): Any {
+        return this.authService.itscSSOServiceValidate(ticket);
     }
 }
