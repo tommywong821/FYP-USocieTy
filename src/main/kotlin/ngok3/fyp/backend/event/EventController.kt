dd5fab55686f0c93d0b83c1d747d5f19e84a1c01
Tommy Wong <tommywong821@gmail.com>
2022-10-15 23:06:46 +0800

    init get soc event endpoint

---
+++ src/main/kotlin/ngok3/fyp/backend/event/EventController.kt
@@ -0,0 +1,23 @@
+package ngok3.fyp.backend.event
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/event")
+class EventController(
+    @Autowired val eventService: EventService
+) {
+    @GetMapping
+    fun getAllSocietyEvent(
+        @RequestParam("sid", required = false, defaultValue = "") sid: String,
+        @RequestParam("pageNum", required = false, defaultValue = "10") pageNum: Int,
+        @RequestParam("pageSize", required = false, defaultValue = "0") pageSize: Int
+    ) {
+        print("sid: $sid pageSize: $pageSize pageNum: $pageNum ")
+        return eventService.getAllSocietyEvent(sid, pageNum, pageSize)
+    }
+}
