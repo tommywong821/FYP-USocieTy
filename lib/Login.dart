0b309ebda09f1151e99708aa7123b13fa3f75f0b
Tommy Wong <tommywong821@gmail.com>
2022-10-23 15:32:25 +0800

    add Azure ad login with secruty store

---
+++ lib/Login.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Login extends StatelessWidget {
+  final loginAction;
+  final String loginError;
+
+  const Login(this.loginAction, this.loginError);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        ElevatedButton(
+          onPressed: () {
+            loginAction();
+          },
+          child: Text('Login'),
+        ),
+        Text(loginError ?? '')
+      ],
+    );
+  }
+}
