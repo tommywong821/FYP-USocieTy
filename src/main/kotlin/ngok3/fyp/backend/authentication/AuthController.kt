af60f1694a3d9129d6e4640028dea87f90da6c73
Tommy Wong <tommywong821@gmail.com>
2022-11-02 15:44:07 +0800

    add user token api

--- src/main/kotlin/ngok3/fyp/backend/authentication/AuthController.kt
+++ src/main/kotlin/ngok3/fyp/backend/authentication/AuthController.kt
@@ -1,6 +1,7 @@
 package ngok3.fyp.backend.authentication
 
 import ngok3.fyp.backend.authentication.model.CasServiceResponse
+import ngok3.fyp.backend.authentication.model.UserToken
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.*
 import javax.servlet.http.HttpServletResponse
@@ -29,5 +30,10 @@ class AuthController(
     @PostMapping("/login")
     fun ITSCROPC(@RequestBody userinfo: Map<String, String>): String {
         return authService.ROPCLogin(userinfo)
+    }
+
+    @PostMapping("/mobileLogin")
+    fun validateMobileLogin(@RequestBody userInfo: Map<String, String>): UserToken {
+        return authService.validateMobileLogin(userInfo)
     }
 }
