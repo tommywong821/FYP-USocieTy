c2c864ae83ff6b6501ae6c9167bf358bbf130f76
Tommy Wong <tommywong821@gmail.com>
2023-01-08 22:42:04 +0800

    return EnrolledEventDto

---
+++ src/main/kotlin/ngok3/fyp/backend/enrolled_event_record/EnrolledEventDto.kt
@@ -0,0 +1,28 @@
+package ngok3.fyp.backend.enrolled_event_record
+
+import java.io.Serializable
+import java.time.format.DateTimeFormatter
+
+data class EnrolledEventDto(
+    val name: String? = "",
+    val poster: String? = "",
+    val location: String? = "",
+    val startDate: String? = "",
+    val endDate: String? = "",
+    val category: String? = "",
+    val status: String? = "",
+) : Serializable {
+    constructor(enrolledEventRecordEntity: EnrolledEventRecordEntity) : this(
+        enrolledEventRecordEntity.eventEntity?.name,
+        enrolledEventRecordEntity.eventEntity?.poster,
+        enrolledEventRecordEntity.eventEntity?.location,
+        enrolledEventRecordEntity.eventEntity?.startDate?.format(
+            DateTimeFormatter.ofPattern("yyyy-MM-dd")
+        ),
+        enrolledEventRecordEntity.eventEntity?.endDate?.format(
+            DateTimeFormatter.ofPattern("yyyy-MM-dd")
+        ),
+        enrolledEventRecordEntity.eventEntity?.category,
+        enrolledEventRecordEntity.status?.status
+    )
+}
--- src/main/kotlin/ngok3/fyp/backend/enrolled_event_record/EnrolledEventRepository.kt
+++ src/main/kotlin/ngok3/fyp/backend/enrolled_event_record/EnrolledEventRepository.kt
@@ -1,6 +1,19 @@
 package ngok3.fyp.backend.enrolled_event_record
 
+import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
+import java.time.LocalDateTime
 
 interface EnrolledEventRepository : CrudRepository<EnrolledEventRecordEntity, EnrolledEventRecordKey> {
+
+    @Query(
+        """select e from EnrolledEventRecordEntity e
+where e.studentEntity.itsc = ?1 and e.eventEntity.startDate >= ?2
+order by e.eventEntity.startDate"""
+    )
+    fun findByStudentEntity_ItscAndEventEntity_StartDateGreaterThanEqualOrderByEventEntity_StartDateAsc(
+        itsc: String,
+        startDate: LocalDateTime
+    ): List<EnrolledEventRecordEntity>
+
 }
