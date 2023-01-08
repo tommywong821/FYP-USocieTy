75466b68e2c5672b5a97b488bc1ee00f106f538e
Tommy Wong <tommywong821@gmail.com>
2023-01-08 23:31:00 +0800

    remove old test case

--- src/test/kotlin/ngok3/fyp/backend/event/EventServiceTest.kt
+++ src/test/kotlin/ngok3/fyp/backend/event/EventServiceTest.kt
@@ -6,10 +6,9 @@ import io.mockk.mockkStatic
 import ngok3.fyp.backend.enrolled_event_record.EnrolledEventRepository
 import ngok3.fyp.backend.student.MockStudentRepository
 import ngok3.fyp.backend.student.StudentRepository
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions.assertIterableEquals
 import org.junit.jupiter.api.Test
 import java.time.LocalDateTime
-import java.util.*
 
 class EventServiceTest {
 
@@ -34,7 +33,6 @@ class EventServiceTest {
         } returns mockEventRepository.withoutSidTestEventPage
 
         val allEvent: List<EventDto> = eventService.getAllSocietyEvent(
-            "",
             mockEventRepository.testPageNumWithoutSid,
             mockEventRepository.testPageSizeWithoutSid
         )
@@ -42,49 +40,6 @@ class EventServiceTest {
         val expectedResult = mockEventRepository.allTestEventList.subList(
             mockEventRepository.testPageNumWithoutSid,
             mockEventRepository.testPageSizeWithoutSid
-        ).map { eventEntity ->
-            EventDto(eventEntity)
-        }
-
-        assertIterableEquals(allEvent, expectedResult)
-    }
-
-    @Test
-    fun `should get all event with invalid sid`() {
-        val invalidItsc: String = "invalid"
-
-        every { studentRepository.findByItsc(invalidItsc) } returns Optional.empty()
-
-        val exception: Exception = assertThrows(Exception::class.java) {
-            eventService.getAllSocietyEvent(
-                invalidItsc,
-                mockEventRepository.testPageNumWithoutSid,
-                mockEventRepository.testPageSizeWithoutSid
-            )
-        }
-
-        assertEquals("Student with $invalidItsc not found", exception.message)
-    }
-
-    @Test
-    fun `should get all event with valid sid`() {
-        every { studentRepository.findByItsc(mockStudentRepository.testItsc) } returns Optional.of(mockStudentRepository.testStudentEntity)
-        every {
-            eventRepository.findBySocietyEntity_EnrolledSocietyRecordEntities_StudentEntityOrderByApplyDeadlineAsc(
-                mockStudentRepository.testStudentEntity,
-                mockEventRepository.testPageableWithSid
-            )
-        } returns mockEventRepository.withSidTestEventPage
-
-        val allEvent: List<EventDto> = eventService.getAllSocietyEvent(
-            mockStudentRepository.testItsc,
-            mockEventRepository.testPageNumWithSid,
-            mockEventRepository.testPageSizeWithSid
-        )
-
-        val expectedResult = mockEventRepository.allTestEventList.subList(
-            mockEventRepository.testFromIndex,
-            mockEventRepository.testToIndex
         ).map { eventEntity ->
             EventDto(eventEntity)
         }
