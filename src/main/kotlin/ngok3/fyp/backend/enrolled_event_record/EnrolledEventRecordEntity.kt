a50a4aaf8ca768367457980a9b0cc959285943e4
Tommy Wong <tommywong821@gmail.com>
2023-01-08 13:52:22 +0800

    change entity code format

--- src/main/kotlin/ngok3/fyp/backend/enrolled_event_record/EnrolledEventRecordEntity.kt
+++ src/main/kotlin/ngok3/fyp/backend/enrolled_event_record/EnrolledEventRecordEntity.kt
@@ -12,16 +12,20 @@ open class EnrolledEventRecordEntity(
     @EmbeddedId
     open var id: EnrolledEventRecordKey? = null,
 
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status")
+    open var status: EnrolledEventStatus? = null
+) {
     @ManyToOne
     @MapsId("studentUuid")
     @JoinColumn(name = "student_entity_uuid")
-    open var studentEntity: StudentEntity? = null,
+    open var studentEntity: StudentEntity? = null
 
     @ManyToOne
     @MapsId("eventUuid")
     @JoinColumn(name = "event_entity_uuid")
     open var societyEntity: EventEntity? = null
-) {
+
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (other == null || Hibernate.getClass(this) != Hibernate.getClass(other)) return false
