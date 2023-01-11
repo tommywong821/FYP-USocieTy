bd44b1ee4654b668b0e99799053ab491b2126e63
Tommy Wong <tommywong821@gmail.com>
2023-01-11 21:16:37 +0800

    set up spring web security

--- src/main/kotlin/ngok3/fyp/backend/healthcheck/HealthCheckController.kt
+++ src/main/kotlin/ngok3/fyp/backend/healthcheck/HealthCheckController.kt
@@ -1,5 +1,6 @@
 package ngok3.fyp.backend.healthcheck
 
+import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.CookieValue
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -8,9 +9,21 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/health")
 class HealthCheckController {
-    @GetMapping
-    fun healthCheck(@CookieValue(name = "token") token: String): String {
+    @GetMapping("/cookie")
+    fun healthCheck(@CookieValue(name = "token", required = false) token: String): String {
         print(token)
+        return "cookie OK"
+    }
+
+    @GetMapping("/testNoRole")
+    @PreAuthorize("hasRole('test')")
+    fun healthCheck(): String {
+        return "OK"
+    }
+
+    @GetMapping("/testRole")
+    @PreAuthorize("hasRole('ROLE_STUDENT')")
+    fun healthCheck2(): String {
         return "OK"
     }
 }
