f1732626285daf81ef8a7063488b6eb4158dbd59
Tommy Wong <tommywong821@gmail.com>
2023-01-26 12:44:57 +0800

    add finance table

--- src/app/finance/finance-table/finance-table.component.ts
+++ src/app/finance/finance-table/finance-table.component.ts
@@ -1,15 +1,76 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
+export interface Data {
+  id: number;
+  date: string;
+  amount: number;
+  description: string;
+  editBy: string;
+  disabled: boolean;
+}
 @Component({
   selector: 'app-finance-table',
   templateUrl: './finance-table.component.html',
-  styleUrls: ['./finance-table.component.scss']
+  styleUrls: ['./finance-table.component.scss'],
 })
 export class FinanceTableComponent implements OnInit {
+  checked = false;
+  loading = false;
+  indeterminate = false;
+  listOfData: readonly Data[] = [];
+  listOfCurrentPageData: readonly Data[] = [];
+  setOfCheckedId = new Set<number>();
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.listOfData = new Array(100).fill(0).map((_, index) => ({
+      id: index,
+      date: `Edward King ${index}`,
+      amount: 32,
+      description: `London, Park Lane no. ${index}`,
+      editBy: `edit by ${index}`,
+      disabled: index % 2 === 0,
+    }));
   }
 
+  updateCheckedSet(id: number, checked: boolean): void {
+    if (checked) {
+      this.setOfCheckedId.add(id);
+    } else {
+      this.setOfCheckedId.delete(id);
+    }
+  }
+
+  onCurrentPageDataChange(listOfCurrentPageData: readonly Data[]): void {
+    this.listOfCurrentPageData = listOfCurrentPageData;
+    this.refreshCheckedStatus();
+  }
+
+  refreshCheckedStatus(): void {
+    const listOfEnabledData = this.listOfCurrentPageData.filter(({disabled}) => !disabled);
+    this.checked = listOfEnabledData.every(({id}) => this.setOfCheckedId.has(id));
+    this.indeterminate = listOfEnabledData.some(({id}) => this.setOfCheckedId.has(id)) && !this.checked;
+  }
+
+  onAllChecked(checked: boolean): void {
+    this.listOfCurrentPageData.filter(({disabled}) => !disabled).forEach(({id}) => this.updateCheckedSet(id, checked));
+    this.refreshCheckedStatus();
+  }
+
+  sendRequest(): void {
+    this.loading = true;
+    const requestData = this.listOfData.filter(data => this.setOfCheckedId.has(data.id));
+    console.log(requestData);
+    setTimeout(() => {
+      this.setOfCheckedId.clear();
+      this.refreshCheckedStatus();
+      this.loading = false;
+    }, 1000);
+  }
+
+  onItemChecked(id: number, checked: boolean): void {
+    this.updateCheckedSet(id, checked);
+    this.refreshCheckedStatus();
+  }
 }
