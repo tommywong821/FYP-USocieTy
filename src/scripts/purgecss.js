d508251a03d6eda97f30b1d13bb1bc3cd6e183ca
Tommy Wong <tommywong821@gmail.com>
2023-01-29 21:55:15 +0800

    removed unused css postbuild

---
+++ src/scripts/purgecss.js
@@ -0,0 +1,48 @@
+const exec = require('child_process').exec;
+const fs = require('fs');
+const path = require('path');
+
+// find the styles css file
+const files = getFilesFromPath('./dist/ngok3fyp-frontend', '.css');
+let data = [];
+
+if (!files && files.length <= 0) {
+  console.log('cannot find style files to purge');
+  return;
+}
+
+for (let f of files) {
+  // get original file size
+  const originalSize = getFilesizeInKiloBytes('./dist/ngok3fyp-frontend/' + f) + 'kb';
+  var o = {file: f, originalSize: originalSize, newSize: ''};
+  data.push(o);
+}
+
+console.log('Run PurgeCSS...');
+
+exec(
+  'purgecss -css dist/ngok3fyp-frontend/*.css --content dist/ngok3fyp-frontend/index.html dist/ngok3fyp-frontend/*.js -o dist/ngok3fyp-frontend/',
+  function (error, stdout, stderr) {
+    console.log('PurgeCSS done');
+    console.log();
+
+    for (let d of data) {
+      // get new file size
+      const newSize = getFilesizeInKiloBytes('./dist/ngok3fyp-frontend/' + d.file) + 'kb';
+      d.newSize = newSize;
+    }
+
+    console.table(data);
+  }
+);
+
+function getFilesizeInKiloBytes(filename) {
+  var stats = fs.statSync(filename);
+  var fileSizeInBytes = stats.size / 1024;
+  return fileSizeInBytes.toFixed(2);
+}
+
+function getFilesFromPath(dir, extension) {
+  let files = fs.readdirSync(dir);
+  return files.filter(e => path.extname(e).toLowerCase() === extension);
+}
