7d8062642c16b4baf6fa92554e37d20afde73cce
Tommy Wong <tommywong821@gmail.com>
2023-01-30 16:37:58 +0800

    init finance table

--- src/main/kotlin/ngok3/fyp/backend/operation/society/SocietyEntity.kt
+++ src/main/kotlin/ngok3/fyp/backend/operation/society/SocietyEntity.kt
@@ -1,10 +1,9 @@
 package ngok3.fyp.backend.operation.society
 
 import ngok3.fyp.backend.operation.enrolled_society_record.EnrolledSocietyRecordEntity
+import ngok3.fyp.backend.operation.finance.FinanceEntity
 import ngok3.fyp.backend.util.entity.BaseEntity
-import javax.persistence.Entity
-import javax.persistence.OneToMany
-import javax.persistence.Table
+import javax.persistence.*
 
 @Entity
 @Table(name = "society")
@@ -15,4 +14,12 @@ open class SocietyEntity(
 
     @OneToMany(mappedBy = "societyEntity")
     open var enrolledSocietyRecordEntities: MutableSet<EnrolledSocietyRecordEntity> = mutableSetOf()
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+        name = "society_finance",
+        joinColumns = [JoinColumn(name = "society_uuid")],
+        inverseJoinColumns = [JoinColumn(name = "finance_uuid")]
+    )
+    open var financeRecords: MutableSet<FinanceEntity> = mutableSetOf()
 }
