c73d9e3cf9ef21dd06568e3585cd930786b49d1a
Tommy Wong <tommywong821@gmail.com>
2023-02-03 23:36:24 +0800

    add swagger description

--- src/main/kotlin/ngok3/fyp/backend/operation/finance/FinanceController.kt
+++ src/main/kotlin/ngok3/fyp/backend/operation/finance/FinanceController.kt
@@ -1,5 +1,8 @@
 package ngok3.fyp.backend.operation.finance
 
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.Parameter
+import io.swagger.v3.oas.annotations.Parameters
 import ngok3.fyp.backend.operation.finance.model.FinanceTableDto
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
@@ -12,6 +15,13 @@ import org.springframework.web.bind.annotation.RestController
 class FinanceController(
     @Autowired val financeService: FinanceService
 ) {
+    @Operation(summary = "get all finance record with society name returning in table dto")
+    @Parameters(
+        Parameter(
+            name = "fromDate",
+            description = "Date Format: dd-MM-yyyy \n\n eg: 02-03-2023 === 2nd February 2023"
+        ), Parameter(name = "toDate", description = "Date Format: dd-MM-yyyy \n\n eg: 03-03-2023 === 3rd February 2023")
+    )
     @GetMapping("/table")
     fun getFinanceTableData(
         @RequestParam("societyName") societyName: String,
