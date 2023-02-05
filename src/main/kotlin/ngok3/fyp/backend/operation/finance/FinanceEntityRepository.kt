51bf8b6015ff3b73d4812b49f07b4a5f52e049cb
Tommy Wong <tommywong821@gmail.com>
2023-02-05 13:23:42 +0800

    TDD: add get finance pie chart api

--- src/main/kotlin/ngok3/fyp/backend/operation/finance/FinanceEntityRepository.kt
+++ src/main/kotlin/ngok3/fyp/backend/operation/finance/FinanceEntityRepository.kt
@@ -1,5 +1,6 @@
 package ngok3.fyp.backend.operation.finance;
 
+import ngok3.fyp.backend.operation.finance.model.FinanceChartDto
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 import java.time.LocalDateTime
@@ -7,12 +8,12 @@ import java.util.*
 
 interface FinanceEntityRepository : JpaRepository<FinanceEntity, UUID> {
 
-//    @Query("select new ngok3.fyp.backend.operation.finance.model.FinanceChartDto(f.category, count(f.amount)) from FinanceEntity f where f.societyEntity.name = ?1 and f.date >= ?2 and f.date <= ?3 group by f.category")
-//    fun findFinanceChartData(
-//        name: String,
-//        fromDate: LocalDateTime,
-//        toDate: LocalDateTime
-//    ): List<FinanceChartDto>
+    @Query("select new ngok3.fyp.backend.operation.finance.model.FinanceChartDto(f.category, count(f.amount)) from FinanceEntity f where f.societyEntity.name = ?1 and f.date >= ?2 and f.date <= ?3 group by f.category")
+    fun findFinanceChartData(
+        name: String,
+        fromDate: LocalDateTime,
+        toDate: LocalDateTime
+    ): List<FinanceChartDto>
 
 
     @Query(
