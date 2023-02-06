81c2d16bca560f1b9d714280cdb087b7db224201
Tommy Wong <tommywong821@gmail.com>
2023-02-06 17:40:13 +0800

    change create finance form group to form array

--- src/app/finance/finance-create/finance-create.component.ts
+++ src/app/finance/finance-create/finance-create.component.ts
@@ -1,14 +1,8 @@
 import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import {Form, FormArray, FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 import {IconProp} from '@fortawesome/fontawesome-svg-core';
 import {faMinus} from '@fortawesome/free-solid-svg-icons';
 
-export interface IFinanceRecordForm {
-  id: number;
-  amount: string;
-  description: string;
-  date: string;
-}
 @Component({
   selector: 'app-finance-create',
   templateUrl: './finance-create.component.html',
@@ -16,13 +10,20 @@ export interface IFinanceRecordForm {
 })
 export class FinanceCreateComponent implements OnInit {
   validateForm!: FormGroup;
-  listOfFinanceRecord: Array<IFinanceRecordForm> = [];
   faMinus: IconProp = faMinus;
 
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder) {
+    this.validateForm = this.fb.group({
+      societyName: '',
+      financeRecords: this.fb.array([]),
+    });
+  }
+
+  get financeRecords(): FormArray {
+    return this.validateForm.get('financeRecords') as FormArray;
+  }
 
   ngOnInit(): void {
-    this.validateForm = this.fb.group({});
     this.addField();
   }
 
@@ -30,47 +31,33 @@ export class FinanceCreateComponent implements OnInit {
     if (e) {
       e.preventDefault();
     }
-    const id =
-      this.listOfFinanceRecord.length > 0 ? this.listOfFinanceRecord[this.listOfFinanceRecord.length - 1].id + 1 : 0;
-
-    const control = {
-      id,
-      date: `${id}date`,
-      amount: `${id}amount`,
-      description: `${id}description`,
-    };
-    const index = this.listOfFinanceRecord.push(control);
-    console.log(this.listOfFinanceRecord[this.listOfFinanceRecord.length - 1]);
-    //Validate user input
-    this.validateForm.addControl(
-      this.listOfFinanceRecord[index - 1].amount,
-      new FormControl(null, [Validators.required, Validators.pattern('^[0-9]*$')])
-    );
-    this.validateForm.addControl(this.listOfFinanceRecord[index - 1].date, new FormControl(null, Validators.required));
-    this.validateForm.addControl(
-      this.listOfFinanceRecord[index - 1].description,
-      new FormControl(null, Validators.required)
+    this.financeRecords.push(
+      // //Validate user input
+      this.fb.group({
+        amount: new FormControl(null, [Validators.required, Validators.pattern('^[0-9]*$')]),
+        description: new FormControl(null, Validators.required),
+        date: new FormControl(null, Validators.required),
+      })
     );
   }
 
-  removeField(item: IFinanceRecordForm, e: MouseEvent): void {
-    e.preventDefault();
-    if (this.listOfFinanceRecord.length > 1) {
-      const index = this.listOfFinanceRecord.indexOf(item);
-      this.listOfFinanceRecord.splice(index, 1);
-      console.log(this.listOfFinanceRecord);
-      this.validateForm.removeControl(item.amount);
-      this.validateForm.removeControl(item.date);
-      this.validateForm.removeControl(item.description);
+  removeFinanceRecrod(index: number): void {
+    if (this.financeRecords.value.length > 1) {
+      (<FormArray>this.validateForm.get('financeRecords')).removeAt(index);
     }
   }
 
   submitForm(): void {
     if (this.validateForm.valid) {
       console.log('submit', this.validateForm.value);
-      //TODO group object: current onlt fields
+      //change date format to mm/dd/2023
+      this.validateForm.value.financeRecords.forEach((financeRecord: any) => {
+        financeRecord.date = financeRecord.date.toLocaleDateString();
+      });
+      console.log('modify', this.validateForm.value);
     } else {
-      Object.values(this.validateForm.controls).forEach(control => {
+      alert('You must fill all the fields');
+      Object.values(this.financeRecords.controls).forEach(control => {
         if (control.invalid) {
           control.markAsDirty();
           control.updateValueAndValidity({onlySelf: true});
