3138a4a79ef8edce449e1d11bcddb3befd33f968
Tommy Wong <tommywong821@gmail.com>
2023-02-20 00:45:01 +0800

    service: finish delete event api endpoint

--- src/test/kotlin/ngok3/fyp/backend/operation/event/EventServiceTest.kt
+++ src/test/kotlin/ngok3/fyp/backend/operation/event/EventServiceTest.kt
@@ -3,22 +3,27 @@ package ngok3.fyp.backend.operation.event
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.mockkStatic
+import io.mockk.verify
+import ngok3.fyp.backend.controller.authentication.model.MockAuthRepository
 import ngok3.fyp.backend.operation.enrolled_event_record.EnrolledEventRecordRepository
+import ngok3.fyp.backend.operation.enrolled_event_record.EnrolledStatus
+import ngok3.fyp.backend.operation.enrolled_society_record.EnrolledSocietyRecordEntity
 import ngok3.fyp.backend.operation.enrolled_society_record.EnrolledSocietyRecordRepository
 import ngok3.fyp.backend.operation.event.dto.EventDto
 import ngok3.fyp.backend.operation.s3.S3Service
+import ngok3.fyp.backend.operation.society.SocietyEntity
 import ngok3.fyp.backend.operation.society.SocietyRepository
 import ngok3.fyp.backend.operation.student.StudentRepository
-import ngok3.fyp.backend.student.MockStudentRepository
 import ngok3.fyp.backend.util.JWTUtil
 import org.junit.jupiter.api.Assertions.assertIterableEquals
 import org.junit.jupiter.api.Test
 import java.time.LocalDateTime
+import java.util.*
 
 class EventServiceTest {
 
     private val mockEventRepository: MockEventRepository = MockEventRepository()
-    private val mockStudentRepository: MockStudentRepository = MockStudentRepository()
+    private val mockAuthRepository: MockAuthRepository = MockAuthRepository()
 
     private val eventRepository: EventRepository = mockk()
     private val studentRepository: StudentRepository = mockk()
@@ -62,5 +67,41 @@ class EventServiceTest {
         }
 
         assertIterableEquals(allEvent, expectedResult)
+    }
+
+    @Test
+    fun `should delete event with event id`() {
+        val uuid: String = UUID.randomUUID().toString()
+        val mockEventEntity: EventEntity = EventEntity()
+        mockEventEntity.societyEntity = SocietyEntity(name = mockAuthRepository.testSocietyName)
+
+        every {
+            eventRepository.findById(UUID.fromString(uuid))
+        } returns Optional.of(mockEventEntity)
+
+        every {
+            enrolledSocietyRecordRepository.findByItscAndSocietyNameAndEnrolledStatus(
+                mockAuthRepository.validUserItsc,
+                mockAuthRepository.testSocietyName,
+                EnrolledStatus.SUCCESS
+            )
+        } returns Optional.of(EnrolledSocietyRecordEntity())
+
+        every {
+            eventRepository.deleteById(UUID.fromString(uuid))
+        } returns Unit
+
+        eventService.deleteEvent(mockAuthRepository.validUserCookieToken, uuid)
+
+        verify(exactly = 1) { eventRepository.deleteById(UUID.fromString(uuid)) }
+        verify(exactly = 1) { eventRepository.findById(UUID.fromString(uuid)) }
+        verify(exactly = 1) {
+            enrolledSocietyRecordRepository.findByItscAndSocietyNameAndEnrolledStatus(
+                mockAuthRepository.validUserItsc,
+                mockAuthRepository.testSocietyName,
+                EnrolledStatus.SUCCESS
+            )
+        }
+
     }
 }
