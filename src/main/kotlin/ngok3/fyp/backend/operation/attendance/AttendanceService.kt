ab48b801f5ebc3d0616ab523f1f16ae16dcfac94
Tommy Wong <tommywong821@gmail.com>
2023-02-27 22:02:18 +0800

    remove key log

--- src/main/kotlin/ngok3/fyp/backend/operation/attendance/AttendanceService.kt
+++ src/main/kotlin/ngok3/fyp/backend/operation/attendance/AttendanceService.kt
@@ -3,12 +3,18 @@ package ngok3.fyp.backend.operation.attendance
 import ngok3.fyp.backend.operation.enrolled.EnrolledStatus
 import ngok3.fyp.backend.operation.enrolled.event_record.EnrolledEventRecordKey
 import ngok3.fyp.backend.operation.enrolled.event_record.EnrolledEventRecordRepository
+import ngok3.fyp.backend.operation.event.EventEntity
+import ngok3.fyp.backend.operation.event.EventRepository
+import ngok3.fyp.backend.operation.student.StudentEntity
+import ngok3.fyp.backend.operation.student.StudentRepository
 import org.springframework.stereotype.Service
 import java.util.*
 
 @Service
 class AttendanceService(
     private val enrolledEventRecordRepository: EnrolledEventRecordRepository,
+    private val studentRepository: StudentRepository,
+    private val eventRepository: EventRepository,
     private val attendanceRepository: AttendanceRepository
 ) {
     fun createAttendance(studentId: String, eventId: String) {
@@ -19,12 +25,21 @@ class AttendanceService(
             Exception("Student: $studentId did not enrolled Event: $eventId")
         }
 
+        val studentEntity: StudentEntity = studentRepository.findById(UUID.fromString(studentId)).orElseThrow {
+            throw Exception("Student id: $studentId is not exist")
+        }
+        val eventEntity: EventEntity = eventRepository.findById(UUID.fromString(eventId)).orElseThrow {
+            throw Exception("Event id: $eventId is not exist")
+        }
+
         val attendanceKey: AttendanceKey = AttendanceKey()
         attendanceKey.eventUuid = UUID.fromString(eventId)
         attendanceKey.studentUuid = UUID.fromString(studentId)
 
         val attendanceEntity: AttendanceEntity = AttendanceEntity()
         attendanceEntity.attendanceKey = attendanceKey
+        attendanceEntity.eventEntity = eventEntity
+        attendanceEntity.studentEntity = studentEntity
 
         attendanceRepository.save(attendanceEntity)
     }
