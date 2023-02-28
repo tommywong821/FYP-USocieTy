f167aa966c57e13e4858c5a94dd57549d7333463
Tommy Wong <tommywong821@gmail.com>
2023-02-28 15:12:19 +0800

    finish assign society member api

--- src/main/kotlin/ngok3/fyp/backend/authentication/role/RoleEntityRepository.kt
+++ src/main/kotlin/ngok3/fyp/backend/authentication/role/RoleEntityRepository.kt
@@ -2,11 +2,12 @@ package ngok3.fyp.backend.authentication.role;
 
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
+import java.util.*
 
 interface RoleEntityRepository : JpaRepository<RoleEntity, Int> {
 
 
     @Query("select r from RoleEntity r where r.role = ?1")
-    fun findByRole(role: Role): RoleEntity
+    fun findByRole(role: Role): Optional<RoleEntity>
 
 }
--- src/test/kotlin/ngok3/fyp/backend/operation/society/SocietyServiceTest.kt
+++ src/test/kotlin/ngok3/fyp/backend/operation/society/SocietyServiceTest.kt
@@ -2,7 +2,12 @@ package ngok3.fyp.backend.operation.society
 
 import io.mockk.every
 import io.mockk.mockk
+import io.mockk.verify
+import ngok3.fyp.backend.authentication.role.Role
+import ngok3.fyp.backend.authentication.role.RoleEntity
+import ngok3.fyp.backend.authentication.role.RoleEntityRepository
 import ngok3.fyp.backend.controller.authentication.model.MockAuthRepository
+import ngok3.fyp.backend.operation.enrolled.EnrolledStatus
 import ngok3.fyp.backend.operation.enrolled.society_record.EnrolledSocietyRecordRepository
 import ngok3.fyp.backend.operation.student.StudentDto
 import ngok3.fyp.backend.operation.student.StudentEntity
@@ -10,6 +15,7 @@ import ngok3.fyp.backend.operation.student.StudentRepository
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import java.util.*
 
 @SpringBootTest
 class SocietyServiceTest {
@@ -19,10 +25,13 @@ class SocietyServiceTest {
     private val studentRepository: StudentRepository = mockk(relaxed = true)
     private val enrolledSocietyRecordRepository: EnrolledSocietyRecordRepository = mockk()
 
+    private val roleEntityRepository: RoleEntityRepository = mockk(relaxed = true)
+
     private val societyService: SocietyService = SocietyService(
         societyRepository = societyRepository,
         studentRepository = studentRepository,
-        enrolledSocietyRecordRepository = enrolledSocietyRecordRepository
+        enrolledSocietyRecordRepository = enrolledSocietyRecordRepository,
+        roleEntityRepository = roleEntityRepository
     )
 
     @Test
@@ -41,5 +50,36 @@ class SocietyServiceTest {
 
         assertEquals("asdfg", allMembers[1].itsc)
         assertEquals("nickname 2", allMembers[1].nickname)
+    }
+
+    @Test
+    fun assignSocietyMemberRole() {
+        val studentIdList: List<String> = listOf<String>(
+            "2ac23d21-4cb0-4173-a2fe-de551ec5aa9d",
+            "38153605-ed2c-42e7-947a-9d1731f4bd44"
+        )
+
+        val mockStudentEntityList: List<StudentEntity> = listOf(
+            StudentEntity("qwert", "nickname 1"),
+            StudentEntity("asdfg", "nickname 2"),
+        )
+
+        every {
+            roleEntityRepository.findByRole(Role.ROLE_SOCIETY_MEMBER)
+        } returns Optional.of(RoleEntity(1, Role.ROLE_SOCIETY_MEMBER))
+
+        every {
+            studentRepository.findByIdInAndEnrolledSocietyNameAndEnrollStatus(studentIdList.map { studentIdString ->
+                UUID.fromString(
+                    studentIdString
+                )
+            }.toMutableList(), mockAuthRepository.testSocietyName, EnrolledStatus.SUCCESS)
+        } returns mockStudentEntityList
+
+        every { studentRepository.saveAll(mockStudentEntityList) } returns mockStudentEntityList
+
+        societyService.assignSocietyMemberRole(mockAuthRepository.testSocietyName, studentIdList)
+
+        verify(exactly = 1) { studentRepository.saveAll(mockStudentEntityList) }
     }
 }
