caa2401e3f033e6089b265b27efcaafe4969fbea
Tommy Wong <tommywong821@gmail.com>
2023-03-06 21:43:21 +0800

    init get all attendance api endpoint

--- src/test/kotlin/ngok3/fyp/backend/operation/attendance/AttendanceControllerTest.kt
+++ src/test/kotlin/ngok3/fyp/backend/operation/attendance/AttendanceControllerTest.kt
@@ -2,6 +2,7 @@ package ngok3.fyp.backend.operation.attendance
 
 import com.ninjasquad.springmockk.MockkBean
 import io.mockk.every
+import ngok3.fyp.backend.operation.attendance.model.StudentAttendanceDto
 import ngok3.fyp.backend.util.RSAUtil
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
@@ -9,7 +10,9 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.get
 import org.springframework.test.web.servlet.post
+import java.time.LocalDateTime
 import java.util.*
 
 @SpringBootTest
@@ -38,5 +41,42 @@ class AttendanceControllerTest @Autowired constructor(
             content =
                 "{\"data\":\"dc3XrBxZHE0urCUzKlehtgXvLwl93WIgb+udouUxs2uxT56+ptMptD9I3VNxL3fTXGvyIXDlU6zVZcRuaMfV/6OBuCIylFigdChzngIg7WuEQ0kgtvP4aG/6eqyEz8eOdW/4czE+kxwYKq1yhC3y9PPwwY6fyhr0PZmV5jftXN4=\"}"
         }.andDo { print() }.andExpect { status { isCreated() } }
+    }
+
+    @Test
+    fun `should get all attendance`() {
+        val studentAttendanceDtoList: List<StudentAttendanceDto> = listOf(
+            StudentAttendanceDto(UUID.randomUUID().toString(), "nickname 1", LocalDateTime.now().toString()),
+            StudentAttendanceDto(UUID.randomUUID().toString(), "nickname 2", LocalDateTime.now().toString()),
+        )
+
+        every {
+            attendanceService.getAllAttendance()
+        } returns studentAttendanceDtoList
+
+        mockMvc.get("/attendance") {
+            headers {
+                contentType = MediaType.APPLICATION_JSON
+            }
+        }.andDo { print() }
+            .andExpect {
+                status { isOk() }
+                content { contentType(MediaType.APPLICATION_JSON) }
+                jsonPath("$") {
+                    isArray()
+                }
+                jsonPath("$.size()") {
+                    value(studentAttendanceDtoList.size)
+                }
+                jsonPath("$[*].studentUuid") {
+                    value(studentAttendanceDtoList.map { studentAttendance -> studentAttendance.studentUuid })
+                }
+                jsonPath("$[*].studentNickname") {
+                    value(studentAttendanceDtoList.map { studentAttendance -> studentAttendance.studentNickname })
+                }
+                jsonPath("$[*].attendanceCreatedAt") {
+                    value(studentAttendanceDtoList.map { studentAttendance -> studentAttendance.attendanceCreatedAt })
+                }
+            }
     }
 }
