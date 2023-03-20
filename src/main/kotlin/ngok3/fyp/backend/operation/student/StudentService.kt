e9abdd5cae47afd6deb912d12347ca1bf479df13
Tommy Wong <tkwongax@connect.ust.hk>
2023-03-20 23:51:34 +0800

    create getAllEventWithSocietyMember

--- src/main/kotlin/ngok3/fyp/backend/operation/student/StudentService.kt
+++ src/main/kotlin/ngok3/fyp/backend/operation/student/StudentService.kt
@@ -1,14 +1,22 @@
 package ngok3.fyp.backend.operation.student
 
 import ngok3.fyp.backend.authentication.student_role.StudentRoleEntity
+import ngok3.fyp.backend.operation.event.EventEntity
+import ngok3.fyp.backend.operation.event.EventRepository
+import ngok3.fyp.backend.operation.event.dto.EventDto
+import ngok3.fyp.backend.util.JWTUtil
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.data.domain.PageRequest
 import org.springframework.stereotype.Service
 import java.util.*
 
 @Service
 class StudentService(
     private val studentRepository: StudentRepository,
+    private val jwtUtil: JWTUtil, private val eventRepository: EventRepository,
 ) {
-
+    @Value("\${aws.bucket.domain}")
+    val s3BucketDomain: String = ""
     fun getStudentProfile(itsc: String, uuid: String, cardId: String): StudentDto {
         val studentEntity: StudentEntity = when {
             itsc.isNotBlank() -> studentRepository.findByItsc(itsc)
@@ -23,5 +31,20 @@ class StudentService(
             studentEntity.studentRoleEntities.map { studentRoleEntity: StudentRoleEntity -> studentRoleEntity.societyEntity.name }
 
         return StudentDto(studentEntity, enrolledSocietyList)
+    }
+
+    fun getAllEventWithSocietyMember(studentId: String, pageNum: Int, pageSize: Int): List<EventDto> {
+        val student: StudentEntity = studentRepository.findById(UUID.fromString(studentId)).orElseThrow {
+            Exception("student with id: $studentId is not found")
+        }
+
+        val societyNameList: MutableList<String> =
+            student.studentRoleEntities.map { studentRoleEntity: StudentRoleEntity -> studentRoleEntity.societyEntity.name }
+                .toMutableList()
+
+        return eventRepository.findAllBySocietyNameList(societyNameList, PageRequest.of(pageNum, pageSize))
+            .map { eventEntity: EventEntity ->
+                EventDto().createFromEntity(eventEntity, s3BucketDomain)
+            }
     }
 }
